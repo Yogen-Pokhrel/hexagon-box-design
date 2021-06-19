850f1e68568df6e4973afc33133ce1a3ae84d53c
yogendrapokhrel777@gmail.com <yogendrapokhrel777@gmail.com>
2021-06-19 15:27:35 +0400

    Hexagonal Pattern Css Updated

---
+++ index.php
@@ -0,0 +1,157 @@
+<link rel="stylesheet" type="text/css" href="./css/style.css">
+<div class="RepairsChooser BrandsChooser mt-3 mb-5">
+                        <div class="RepairsChooser__hexContainer BrandsChooser__hexContainer min1200 max1200" style="justify-content:center">
+                                                    <div class="BrandsChooser__hexagonItem color-hex-default RepairItem--replaceFrontBrakePads" role="button" tabindex="0">
+                                <a href="car-service" class="d-block">
+                                <div class="RepairsChooser__repairItem BrandsChooser__chooserItem">
+                                    <div class="RepairsChooser__repairImageContainer BrandsChooser__chooserImageContainer"><img class="RepairsChooser__makeImage BrandsChooser__chooserImage" src="https://servicemycar.ae/smc/front-media/car-logo/acura-icon.jpg" alt=""></div>
+                                    <div class="RepairsChooser__repairLabel BrandsChooser__chooserLabel text-center small-text d-none">acura</div>
+                                </div>
+                                </a>
+                            </div>
+                                                        <div class="BrandsChooser__hexagonItem color-hex-default RepairItem--replaceFrontBrakePads" role="button" tabindex="0">
+                                <a href="alfa-romeo-service-riyadh" class="d-block">
+                                <div class="RepairsChooser__repairItem BrandsChooser__chooserItem">
+                                    <div class="RepairsChooser__repairImageContainer BrandsChooser__chooserImageContainer"><img class="RepairsChooser__makeImage BrandsChooser__chooserImage" src="https://servicemycar.ae/smc/front-media/car-logo/alfa-romeo-icon.jpg" alt=""></div>
+                                    <div class="RepairsChooser__repairLabel BrandsChooser__chooserLabel text-center small-text d-none">alfa romeo</div>
+                                </div>
+                                </a>
+                            </div>
+                                                        <div class="BrandsChooser__hexagonItem color-hex-default RepairItem--replaceFrontBrakePads" role="button" tabindex="0">
+                                <a href="aston-martin-service-riyadh" class="d-block">
+                                <div class="RepairsChooser__repairItem BrandsChooser__chooserItem">
+                                    <div class="RepairsChooser__repairImageContainer BrandsChooser__chooserImageContainer"><img class="RepairsChooser__makeImage BrandsChooser__chooserImage" src="https://servicemycar.ae/smc/front-media/car-logo/aston-martin-icon.jpg" alt=""></div>
+                                    <div class="RepairsChooser__repairLabel BrandsChooser__chooserLabel text-center small-text d-none">aston martin</div>
+                                </div>
+                                </a>
+                            </div>
+                                                        <div class="BrandsChooser__hexagonItem color-hex-default RepairItem--replaceFrontBrakePads" role="button" tabindex="0">
+                                <a href="audi-service-riyadh" class="d-block">
+                                <div class="RepairsChooser__repairItem BrandsChooser__chooserItem">
+                                    <div class="RepairsChooser__repairImageContainer BrandsChooser__chooserImageContainer"><img class="RepairsChooser__makeImage BrandsChooser__chooserImage" src="https://servicemycar.ae/smc/front-media/car-logo/audi-icon.jpg" alt=""></div>
+                                    <div class="RepairsChooser__repairLabel BrandsChooser__chooserLabel text-center small-text d-none">audi</div>
+                                </div>
+                                </a>
+                            </div>
+                                                        <div class="BrandsChooser__hexagonItem color-hex-default RepairItem--replaceFrontBrakePads" role="button" tabindex="0">
+                                <a href="bentley-service-riyadh" class="d-block">
+                                <div class="RepairsChooser__repairItem BrandsChooser__chooserItem">
+                                    <div class="RepairsChooser__repairImageContainer BrandsChooser__chooserImageContainer"><img class="RepairsChooser__makeImage BrandsChooser__chooserImage" src="https://servicemycar.ae/smc/front-media/car-logo/bentley-icon.jpg" alt=""></div>
+                                    <div class="RepairsChooser__repairLabel BrandsChooser__chooserLabel text-center small-text d-none">bentley</div>
+                                </div>
+                                </a>
+                            </div>
+                                                        <div class="BrandsChooser__hexagonItem color-hex-default RepairItem--replaceFrontBrakePads" role="button" tabindex="0">
+                                <a href="bmw-service-riyadh" class="d-block">
+                                <div class="RepairsChooser__repairItem BrandsChooser__chooserItem">
+                                    <div class="RepairsChooser__repairImageContainer BrandsChooser__chooserImageContainer"><img class="RepairsChooser__makeImage BrandsChooser__chooserImage" src="https://servicemycar.ae/smc/front-media/car-logo/bmw-icon.jpg" alt=""></div>
+                                    <div class="RepairsChooser__repairLabel BrandsChooser__chooserLabel text-center small-text d-none">bmw</div>
+                                </div>
+                                </a>
+                            </div>
+                                                        <div class="BrandsChooser__hexagonItem color-hex-default RepairItem--replaceFrontBrakePads" role="button" tabindex="0">
+                                <a href="bugatti-service-riyadh" class="d-block">
+                                <div class="RepairsChooser__repairItem BrandsChooser__chooserItem">
+                                    <div class="RepairsChooser__repairImageContainer BrandsChooser__chooserImageContainer"><img class="RepairsChooser__makeImage BrandsChooser__chooserImage" src="https://servicemycar.ae/smc/front-media/car-logo/bugatti-icon.jpg" alt=""></div>
+                                    <div class="RepairsChooser__repairLabel BrandsChooser__chooserLabel text-center small-text d-none">bugatti</div>
+                                </div>
+                                </a>
+                            </div>
+                                                        <div class="BrandsChooser__hexagonItem color-hex-default RepairItem--replaceFrontBrakePads" role="button" tabindex="0">
+                                <a href="cadillac-service-riyadh" class="d-block">
+                                <div class="RepairsChooser__repairItem BrandsChooser__chooserItem">
+                                    <div class="RepairsChooser__repairImageContainer BrandsChooser__chooserImageContainer"><img class="RepairsChooser__makeImage BrandsChooser__chooserImage" src="https://servicemycar.ae/smc/front-media/car-logo/cadillac-icon.jpg" alt=""></div>
+                                    <div class="RepairsChooser__repairLabel BrandsChooser__chooserLabel text-center small-text d-none">cadillac</div>
+                                </div>
+                                </a>
+                            </div>
+                                                        <div class="BrandsChooser__hexagonItem color-hex-default RepairItem--replaceFrontBrakePads" role="button" tabindex="0">
+                                <a href="chevrolet-service-riyadh" class="d-block">
+                                <div class="RepairsChooser__repairItem BrandsChooser__chooserItem">
+                                    <div class="RepairsChooser__repairImageContainer BrandsChooser__chooserImageContainer"><img class="RepairsChooser__makeImage BrandsChooser__chooserImage" src="https://servicemycar.ae/smc/front-media/car-logo/chevrolet-icon.jpg" alt=""></div>
+                                    <div class="RepairsChooser__repairLabel BrandsChooser__chooserLabel text-center small-text d-none">chevrolet</div>
+                                </div>
+                                </a>
+                            </div>
+                                                        <div class="BrandsChooser__hexagonItem color-hex-default RepairItem--replaceFrontBrakePads" role="button" tabindex="0">
+                                <a href="citroen-service-riyadh" class="d-block">
+                                <div class="RepairsChooser__repairItem BrandsChooser__chooserItem">
+                                    <div class="RepairsChooser__repairImageContainer BrandsChooser__chooserImageContainer"><img class="RepairsChooser__makeImage BrandsChooser__chooserImage" src="https://servicemycar.ae/smc/front-media/car-logo/citroen-icon.jpg" alt=""></div>
+                                    <div class="RepairsChooser__repairLabel BrandsChooser__chooserLabel text-center small-text d-none">citroen</div>
+                                </div>
+                                </a>
+                            </div>
+                                                        <div class="BrandsChooser__hexagonItem color-hex-default RepairItem--replaceFrontBrakePads" role="button" tabindex="0">
+                                <a href="dodge-service-riyadh" class="d-block">
+                                <div class="RepairsChooser__repairItem BrandsChooser__chooserItem">
+                                    <div class="RepairsChooser__repairImageContainer BrandsChooser__chooserImageContainer"><img class="RepairsChooser__makeImage BrandsChooser__chooserImage" src="https://servicemycar.ae/smc/front-media/car-logo/dodge-icon.jpg" alt=""></div>
+                                    <div class="RepairsChooser__repairLabel BrandsChooser__chooserLabel text-center small-text d-none">dodge</div>
+                                </div>
+                                </a>
+                            </div>
+                                                        <div class="BrandsChooser__hexagonItem color-hex-default RepairItem--replaceFrontBrakePads" role="button" tabindex="0">
+                                <a href="ferrari-service-riyadh" class="d-block">
+                                <div class="RepairsChooser__repairItem BrandsChooser__chooserItem">
+                                    <div class="RepairsChooser__repairImageContainer BrandsChooser__chooserImageContainer"><img class="RepairsChooser__makeImage BrandsChooser__chooserImage" src="https://servicemycar.ae/smc/front-media/car-logo/ferrari-icon.jpg" alt=""></div>
+                                    <div class="RepairsChooser__repairLabel BrandsChooser__chooserLabel text-center small-text d-none">ferrari</div>
+                                </div>
+                                </a>
+                            </div>
+                                                        <div class="BrandsChooser__hexagonItem color-hex-default RepairItem--replaceFrontBrakePads" role="button" tabindex="0">
+                                <a href="fiat-service-riyadh" class="d-block">
+                                <div class="RepairsChooser__repairItem BrandsChooser__chooserItem">
+                                    <div class="RepairsChooser__repairImageContainer BrandsChooser__chooserImageContainer"><img class="RepairsChooser__makeImage BrandsChooser__chooserImage" src="https://servicemycar.ae/smc/front-media/car-logo/fiat-icon.jpg" alt=""></div>
+                                    <div class="RepairsChooser__repairLabel BrandsChooser__chooserLabel text-center small-text d-none">fiat</div>
+                                </div>
+                                </a>
+                            </div>
+                                                        <div class="BrandsChooser__hexagonItem color-hex-default RepairItem--replaceFrontBrakePads" role="button" tabindex="0">
+                                <a href="ford-service-riyadh" class="d-block">
+                                <div class="RepairsChooser__repairItem BrandsChooser__chooserItem">
+                                    <div class="RepairsChooser__repairImageContainer BrandsChooser__chooserImageContainer"><img class="RepairsChooser__makeImage BrandsChooser__chooserImage" src="https://servicemycar.ae/smc/front-media/car-logo/ford-icon.jpg" alt=""></div>
+                                    <div class="RepairsChooser__repairLabel BrandsChooser__chooserLabel text-center small-text d-none">ford</div>
+                                </div>
+                                </a>
+                            </div>
+                                                        <div class="BrandsChooser__hexagonItem color-hex-default RepairItem--replaceFrontBrakePads" role="button" tabindex="0">
+                                <a href="geely-service-riyadh" class="d-block">
+                                <div class="RepairsChooser__repairItem BrandsChooser__chooserItem">
+                                    <div class="RepairsChooser__repairImageContainer BrandsChooser__chooserImageContainer"><img class="RepairsChooser__makeImage BrandsChooser__chooserImage" src="https://servicemycar.ae/smc/front-media/car-logo/geely-icon.jpg" alt=""></div>
+                                    <div class="RepairsChooser__repairLabel BrandsChooser__chooserLabel text-center small-text d-none">geely</div>
+                                </div>
+                                </a>
+                            </div>
+                                                        <div class="BrandsChooser__hexagonItem color-hex-default RepairItem--replaceFrontBrakePads" role="button" tabindex="0">
+                                <a href="gmc-service-riyadh" class="d-block">
+                                <div class="RepairsChooser__repairItem BrandsChooser__chooserItem">
+                                    <div class="RepairsChooser__repairImageContainer BrandsChooser__chooserImageContainer"><img class="RepairsChooser__makeImage BrandsChooser__chooserImage" src="https://servicemycar.ae/smc/front-media/car-logo/gmc-icon.jpg" alt=""></div>
+                                    <div class="RepairsChooser__repairLabel BrandsChooser__chooserLabel text-center small-text d-none">gmc</div>
+                                </div>
+                                </a>
+                            </div>
+                                                        <div class="BrandsChooser__hexagonItem color-hex-default RepairItem--replaceFrontBrakePads" role="button" tabindex="0">
+                                <a href="honda-service-riyadh" class="d-block">
+                                <div class="RepairsChooser__repairItem BrandsChooser__chooserItem">
+                                    <div class="RepairsChooser__repairImageContainer BrandsChooser__chooserImageContainer"><img class="RepairsChooser__makeImage BrandsChooser__chooserImage" src="https://servicemycar.ae/smc/front-media/car-logo/honda-icon.jpg" alt=""></div>
+                                    <div class="RepairsChooser__repairLabel BrandsChooser__chooserLabel text-center small-text d-none">honda</div>
+                                </div>
+                                </a>
+                            </div>
+                                                        <div class="BrandsChooser__hexagonItem color-hex-default RepairItem--replaceFrontBrakePads" role="button" tabindex="0">
+                                <a href="hummer-service-riyadh" class="d-block">
+                                <div class="RepairsChooser__repairItem BrandsChooser__chooserItem">
+                                    <div class="RepairsChooser__repairImageContainer BrandsChooser__chooserImageContainer"><img class="RepairsChooser__makeImage BrandsChooser__chooserImage" src="https://servicemycar.ae/smc/front-media/car-logo/hummer-icon.jpg" alt=""></div>
+                                    <div class="RepairsChooser__repairLabel BrandsChooser__chooserLabel text-center small-text d-none">hummer</div>
+                                </div>
+                                </a>
+                            </div>
+                                                        <div class="BrandsChooser__hexagonItem color-hex-default RepairItem--replaceFrontBrakePads" role="button" tabindex="0">
+                                <a href="hyundai-service-riyadh" class="d-block">
+                                <div class="RepairsChooser__repairItem BrandsChooser__chooserItem">
+                                    <div class="RepairsChooser__repairImageContainer BrandsChooser__chooserImageContainer"><img class="RepairsChooser__makeImage BrandsChooser__chooserImage" src="https://servicemycar.ae/smc/front-media/car-logo/hyundai-icon.jpg" alt=""></div>
+                                    <div class="RepairsChooser__repairLabel BrandsChooser__chooserLabel text-center small-text d-none">hyundai</div>
+                                </div>
+                                </a>
+                            </div>
+                                                    </div>
+                    </div>
